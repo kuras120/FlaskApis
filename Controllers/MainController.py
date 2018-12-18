08c879f1966bf7c9f0a6da458e57e23a47d7a5ce
Wojciech <wojciech.kur96@gmail.com>
2018-12-18 23:24:49 +0100

    Set main controller

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -21,5 +21,7 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
+logger.info("--------------START-APP--------------")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- Main.py
+++ Main.py
@@ -3,4 +3,3 @@ from Controllers.MainController import app
 
 if __name__ == '__main__':
     app.run()
-
