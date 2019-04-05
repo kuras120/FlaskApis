4d47f904174b1ea330aff29a40a7ee37b0165228
Wojtek <wojciech.kur96@gmail.com>
2019-04-05 17:56:16 +0200

    Delete MPI route

--- Controllers/UserController.py
+++ Controllers/UserController.py
@@ -1,7 +1,4 @@
-import shlex
-import logging
 import datetime
-import subprocess
 
 from flask import render_template, session, redirect, url_for, current_app, jsonify, request
 
@@ -23,29 +20,6 @@ def index():
             return redirect(url_for('home_controller.index', error=e))
     else:
         return redirect(url_for('home_controller.index', error='You have to log in first.'))
-
-
-@user_controller.route('/prime-zombies', methods=['POST'])
-def release_zombies():
-    logging.getLogger('logger').info('Processing started')
-    data = subprocess.run(shlex.split('mpiexec -n ' +
-                                      request.form['threads'] +
-                                      ' python MPI/StartScript.py ' +
-                                      request.form['numbers'] + ' ' +
-                                      request.form['parts']),
-                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = ''
-    for line in data.stdout.decode('utf-8').split('\n'):
-        if 'output' in line:
-            output = line
-        else:
-            if line:
-                logging.getLogger('logger').info(line.strip())
-
-    logging.getLogger('logger').info('Processing completed')
-    logging.getLogger('logger').info(output)
-
-    return jsonify(output.split(':')[1].strip())
 
 
 @user_controller.route('/logout')
